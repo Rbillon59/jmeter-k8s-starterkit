65b8896045739a6679e2473d25d5eaca0ab64b0e
BECK Jean-Baptiste <jeanbaptiste.beck@orange.com>
2024-01-25 10:10:02 +0100

    correct -i option

--- aws/scripts/run-scenarii-jmeter-tool-to-AWS.sh
+++ aws/scripts/run-scenarii-jmeter-tool-to-AWS.sh
@@ -3,7 +3,7 @@
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -s stack -r region -p profile -i injectors -j scenario"
+   echo "Usage: $0 -s stack -r region -p profile -i injectors -n namespace -j scenario"
    echo -e "\t-s stack"
    echo -e "\t-r region"
    echo -e "\t-p profile"
@@ -13,7 +13,7 @@ helpFunction()
    exit 1 # Exit script after printing help
 }
 
-while getopts "s:r:p:n:" opt
+while getopts "s:r:i:p:n:j:" opt
 do
    case "$opt" in
       s ) stack="$OPTARG" ;;
